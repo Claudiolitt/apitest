ac9db911a89bb9c746a07ae9daad0f8e1993d1ff
Claudio Leite <kalitaldm@gmail.com>
2024-10-25 02:54:01 -0300

    2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+@app.head("/")
 def read_root():
     return {"Hello": "World"}
 
@@ -20,3 +21,4 @@ def scrape_page(option: str):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
